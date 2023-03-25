34f2e494407a445af4f67c4994811a31f452c661
jwrr <jwrr.com@gmail.com>
2023-03-25 10:19:32 -0400

    Add shmoo.cfg config file

---
+++ README.md
@@ -0,0 +1,24 @@
+shmoo
+=====
+
+Status
+------
+
+Pre-Alpha
+
+The Itch
+--------
+
+I needed to run multiple test cases, sweeping across many settings. The first
+implementation was a stack of nested for-loops written in Perl. This is an
+attempt to make the process more generic.
+ 
+Example
+-------
+
+./shmoo.py filter=2 infreq=0.5:0.9:0.1 track=yes
+
+Defaults can be defined in file 'shmoo.cfg'. This file shows syntax
+examples and template format.
+
+ 
--- shmoo.py
+++ shmoo.py
@@ -1,35 +1,11 @@
+#!/usr/bin/env python3
 # shmoo.py
 # python3 shmoo.py
 
 import sys
+import re
 from collections import OrderedDict
 
-
-def getargs(cfg):
-  prog = sys.argv[0]
-  args = sys.argv[1:]
-  for arg in args:
-    parts = arg.split('=',1)
-    numparts = len(parts)
-    k = parts[0]
-    if numparts == 1:
-      cfg[k] = True
-    else:
-      v = parts[1]
-      vparts = v.split(':',2)
-      if (not isfloat(vparts[0])):
-        cfg[k] = v
-      else:
-        vfloats = res = [tofloat(s) for s in vparts]
-        cfg[k] = vfloats
-  return cfg
-
-def isfloat(s):
-  try:
-    float(s)
-    return True
-  except ValueError:
-    return False
 
 def tofloat(s):
   try:
@@ -37,9 +13,101 @@ def tofloat(s):
   except ValueError:
     return s
 
-def printkv(d, name=''):
+def stripall(lines):
+  return [s.strip() for s in lines]
+
+def istrue(s):
+  return s.lower() in ['true', 'on', 'yes', 'y']
+
+def isfalse(s):
+  return s.lower() in ['false', 'off', 'no', 'n']
+
+def isbool(s):
+  return istrue(s) or isfalse(s)
+
+def tobool(s):
+  return istrue(s)
+
+def parsecfg(args):
+  cfg = OrderedDict()
+  arg = ''
+  for arg in args:
+    parts = arg.split('=',1)
+    numparts = len(parts)
+    k = parts[0].strip()
+    if numparts == 1:
+      cfg[k] = True
+    else:
+      v = parts[1].strip()
+      vparts = stripall(v.split(':',2))
+      first = vparts[0]
+      if isbool(first):
+        cfg[k] = tobool(first)
+      elif first.isnumeric():
+        cfg[k] = [tofloat(s) for s in vparts]
+      else:
+        cfg[k] = v
+  return cfg
+
+def slurplines(filename):
+  lines = []
+  with open(filename, 'r') as f:
+    lines = f.read().splitlines()
+  return lines
+
+def removecomments(lines):
+  return [re.sub('#.*', '', s) for s in lines]
+
+def removeblanks(lines):
+  return list(filter(None, lines))
+
+def combinelines(lines, marker="'''"):
+  olines = []
+  combine = False
+  for line in lines:
+    markerfound = re.search(marker, line)
+    if markerfound:
+      line = re.sub(marker, '', line)
+    if combine:
+      oline = oline + line + '\n'
+    else:
+      oline = line
+    if markerfound:
+      combine = not combine
+    if not combine:
+      olines.append(oline)
+  return olines
+
+def readcfg(filename):
+  lines = slurplines(filename)
+  lines = combinelines(lines, "'''")
+  lines = removecomments(lines)
+  lines = removeblanks(lines)
+  cfg = parsecfg(lines)
+  return cfg
+
+def setup():
+  args = parsecfg(sys.argv[1:])
+  filename = 'shmoo.cfg'
+  if 'cfg' in args:
+    filename = args['cfg']
+  cfg = readcfg(filename)
+  cfg.update(args)
+  return cfg
+
+def longestkey(d):
+  klen = 0
   for k,v in d.items():
-    print(f'cfg[{k}] = {v}')
+    if len(k) > klen:
+      klen = len(k)
+  return klen
+
+def printkv(d, name=''):
+  klen = longestkey(d) + 5
+  for k,v in d.items():
+    k = f'cfg[{k}]'
+    k = k.ljust(klen)
+    print(  f'{k} = {v}')
 
 def replace(t, d):
   for k,v in d.items():
@@ -48,28 +116,30 @@ def replace(t, d):
     t = t.replace(fromstr, tostr)
   return t
 
-cfg = OrderedDict(
-  filter     = 15,
-  infreq     = 10,
-  tunefreq   = 10,
-  iamp       = 100,
-  qamp       = 100,
-  track      = True,
-  )
+def abort(s='', err=1):
+  print(f'Error: {s}', file=sys.stderr)
+  sys.exit(err)
 
-template = '''
-  run_script {filter} {infreq} {tunefreq} {iamp} {qamp} track
-'''
 
-cfg = getargs(cfg)
+# def recurse(template, cfg, i=0):
+#   if i in cfg.items()[i]
+
+# ==============================================================
+# ==============================================================
+
+cfg = setup()
 printkv(cfg)
 
+if 'template' not in cfg:
+  abort("'template' not defined")
+
+template = cfg['template']
+print("============================")
 print(template)
 template = replace(template, cfg)
+print("============================")
 print(template)
 
+i = 0
 
-
-
-
-
+# recurse(template, cfg)
